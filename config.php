51e5694055c20c879a59749210d09caeb5f803f3
jaimepa17 <jaime.paredes17@est.unanleon.edu.ni>
2025-05-21 19:05:56 -0600

    cambios_laragon_postgreSQL

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 
 define('SERVIDOR','localhost');
-define('USUARIO','root');
-define('PASSWORD','');
+define('USUARIO','postgres');
+define('PASSWORD','123');
 define('BD','sisgestionescolar');
 
 define('APP_NAME','SISTEMA DE GESTIÓN ESCOLAR');
 define('APP_URL','http://localhost/SIS_ACADEMICO');
 define('KEY_API_MAPS','');
 
-$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+$servidor = "pgsql:dbname=".BD.";host=".SERVIDOR;
 
 try{
-    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    $pdo = new PDO($servidor, USUARIO, PASSWORD);
     // echo"conexion exitosa a la base de datos";
 }catch(PDOException $e){
     print_r( $e);
@@ -24,8 +24,8 @@ class DB {
 
     public static function connect() {
         if (self::$pdo === null) {
-            $servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
-            self::$pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+            $servidor = "pgsql:dbname=".BD.";host=".SERVIDOR;
+            self::$pdo = new PDO($servidor, USUARIO, PASSWORD);
         }
         return self::$pdo;
     }
@@ -39,10 +39,10 @@ class DB {
 }
 
 date_default_timezone_set('America/Managua'); // Establece tu zona horaria
-$fechaHora = date('Y-m-d H:i:s '); 
+$fechaHora = date('Y-m-d H:i:s'); 
 
-$fecha_actual = date(format:'y-m-d ',);
-$mes_actual = date(format:'m ',);
-$dia_actual = date(format:'d ',);
-$anio_actual = date(format:'Y ',);
+$fecha_actual = date('Y-m-d');
+$mes_actual = date('m');
+$dia_actual = date('d');
+$anio_actual = date('Y');
 
--- database/user_admin_table.sql
+++ database/user_admin_table.sql
@@ -1,14 +1,13 @@
 CREATE TABLE usuarios (
-    id_usuario INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario SERIAL PRIMARY KEY,
     nombres VARCHAR(255) NOT NULL,
     cargos VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    fyh_creacion DATETIME NULL,
-    fyh_actualizacion DATETIME NULL,
+    fyh_creacion TIMESTAMP NULL,
+    fyh_actualizacion TIMESTAMP NULL,
     estado VARCHAR(11)
-
-)ENGINE=InnoDB;
+);
 
 INSERT INTO usuarios (nombres,cargos,email,password,fyh_creacion,estado)
-VALUES  ('Jonathan Gutierrez','ADMINISTRADOR','admin@admin.com','$2y$10$Ei6k/uxrptBQF6GiVrW9F.gqg7bZcKuvh5RAk7bciHTE7H8EOTj0q','2025-20-5 16:18:10','1');
+VALUES  ('Jonathan Gutierrez','ADMINISTRADOR','admin@admin.com','$2y$10$Ei6k/uxrptBQF6GiVrW9F.gqg7bZcKuvh5RAk7bciHTE7H8EOTj0q','2025-05-20 16:18:10','1');
